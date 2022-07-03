5e00ec7d5f692965513c6390c7723c0afff5198b
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-03 12:02:06 +0100

    Modify file to fix indentation

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for pos, letter in "abcdefghijklmnopqrstuvwxyz"[::-1]:
   if pos % 2 == 0:
-    letter = chr(ord(letter) -32)
+    letter = chr(ord(letter) - 32)
   print('{}'.format(letter), end="")
