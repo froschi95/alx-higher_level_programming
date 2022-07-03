87792fe56f7e022e4acd919049c2d93e50c49084
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-03 09:54:33 +0100

    Modify task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in 'abcdefghijklmnopqrstuvwxyz':
     if (letter != "q" and letter != "e"):
-        print(f"{letter}", end="")
+        print("{}".format(letter), end="")
