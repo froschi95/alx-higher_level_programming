7277c76f3fa45134d09f4eb4b7f134d9e626d227
Frank Success <frank.c.success@gmail.com>
2022-07-03 02:26:25 -0700

    Add tasks

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    temp_lst = list(str)
+    for i in range(len(temp_lst)):
+        if ord(temp_lst[i]) > 96 and ord(temp_lst[i]) < 123:
+            temp_lst[i] = chr(ord(temp_lst[i]) -32)
+    print("{}".format("".join(temp_lst)))
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last = int(repr(number)[-1])
+    print(f"{last}", end="")
