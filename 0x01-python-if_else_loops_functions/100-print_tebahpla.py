89e2d858d702c4eea0299db1935808d8555c3ad1
Frank Success <frank.c.success@gmail.com>
2022-07-03 04:04:48 -0700

    Update file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for pos, letter in "abcdefghijklmnopqrstuvwxyz"[::-1]:
+for pos, letter in enumerate("abcdefghijklmnopqrstuvwxyz"[::-1]):
   if pos % 2 == 0:
     letter = chr(ord(letter) - 32)
   print('{}'.format(letter), end="")
