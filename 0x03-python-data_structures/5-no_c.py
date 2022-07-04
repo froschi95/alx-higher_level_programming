89ccbf1c1c52d32991b5c1b98c6afdb76169a309
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-04 12:23:13 +0100

    Update Task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for letter in my_string:
-        letter = '' if letter in 'cC' else letter
+        letter = '' if letter in 'cC'
     return my_string
