f9377d05c8ded79b49275b814d0d8eafa9f9a707
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-04 12:42:41 +0100

    Update task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        if my_string[i] in 'cC':
-            my_string[i] = ""
-    return my_string
+    return ''.join(letter for letter in my_string if letter not in 'cC')
