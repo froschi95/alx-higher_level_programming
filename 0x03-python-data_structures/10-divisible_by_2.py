f704bdd9b5e6b7b36fc0a2ee3003b58f7a4a512f
Frank Success <frank.c.success@gmail.com>
2022-07-05 04:35:15 -0700

    Modified

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-<<<<<<< HEAD
-    return [i % 2 == 0 for i in my_list]
-=======
+def divisible_by_2(my_list=[])
     if not my_list:
         return
     return [i % 2 == 0 for i in  my_list]
->>>>>>> 3684b7a61ffe926f4b7934a709395643a45ef589
