25f85173e5fce74e70621646caa4bce10cfb6179
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-07-06 16:35:25 +0100

    Update task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,11 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return 0
     look_up = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    prev = result = 0
+    prev, result = 0, 0
     for ch in roman_string:
-        result += look_up[ch] if look_up[ch] <= prev else look_up[ch] - prev * 2
+        if look_up[ch] <= prev:
+            result += look_up[ch]
+        else:
+            result += look_up[ch] - prev * 2
         prev = look_up[ch]
     return result
