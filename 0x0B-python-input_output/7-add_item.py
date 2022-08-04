f84e8576bfa286d9cd3f38e0bae977a62ea589db
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-08-04 12:48:18 +0100

    Update file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,16 +2,25 @@
 """module to load and save"""
 
 
+import json
 import sys
+import os
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
-        __import__('6-load_from_json_file').load_from_json_file
+save_to_j_f = __import__('5-save_to_json_file').save_to_json_file
+load_from_j_f = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+f_name = 'add_item.json'
+num_args = len(sys.argv)
+
+if not os.path.isfile(f_name):
+    with open(f_name, 'w', encoding='utf-8') as f:
+        f.write('[]')
+
+if num_args > 1:
+    obj = load_from_j_f(f_name)
+    for i in range(1, num_args):
+        obj.append(sys.argv[i])
+    save_to_j_f(obj, f_name)
+
+if __name__ == '__main__':
+    print(f'Usage: ./7-add_item.py <*args>')
