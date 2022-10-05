8ed124233294ccd33bdf77424029ba940ec5cdd5
Frank Success <60326840+froschi95@users.noreply.github.com>
2022-10-05 11:04:55 +0100

    Update task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,8 @@
 const numArgs = process.argv.length - 2
 if (numArgs === 0) {
 	console.log('No argument');
-} else if (numArgs ===1) {
+} else if (numArgs === 1) {
 	console.log('Argument found');
 } else {
-	console.log('Arguments found')
+	console.log('Arguments found');
 }
