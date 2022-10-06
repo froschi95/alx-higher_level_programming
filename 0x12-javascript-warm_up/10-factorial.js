d9ce231b8c41aba530953519d1dec8fe8b63c18a
Frank Success <60326840+froschi95@users.noreply.github.com>
2022-10-06 16:07:09 +0100

    Update: Remove excess space on line 4

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const a = process.argv[2];
 function factorial (a) {
-  if  (isNaN(a) || a === 1) {
+  if (isNaN(a) || a === 1) {
     return (1);
   } else {
     return (a * factorial(a - 1));
