0dd351bdf4cf67c9710b80f63ef0253a402e8cb5
Frank Success <60326840+froschi95@users.noreply.github.com>
2022-10-07 12:11:32 +0100

    Update: Fix syntax issues

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  if (w > 0 && h > 0) {
+  if ((w > 0) && (h > 0)) {
     constructor (w, h) {
       this.width = w;
-      this height = h;
+      this.height = h;
     }
   }
 };
